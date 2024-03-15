1ca817112635f1d2b3b426b6fcff09dafbb6e338
amaanlakdawala <amaanlakdawala301@gmail.com>
2024-03-15 23:35:02 +0530

    functions assignment of javascript

---
+++ js_func_ass.js
@@ -0,0 +1,68 @@
+// Write a JS function that returns the factorial of a number
+// const fact = (num) => {
+//     var result=1;
+//     for(let i=1;i<num+1;i++){
+//         result=result*i
+//     }
+//     return result;
+// }
+// console.log("The factorial of a number is: "+fact(5))
+
+
+
+
+
+
+
+
+// Using Recursion
+
+
+// const fact= (num) => {
+//     if(num==0){
+//         return 1;
+//     }
+//     else{
+//         return num*fact(num-1);
+//     }
+// }
+// console.log(fact(5))
+
+// Write a JavaScript function that print the square and cube of the given number.
+
+// const sum=(num) => {
+//     console.log("Square of the number is: "+num*num);
+//     console.log("Cube of the number is: "+num*num*num);
+// }
+// sum(5)
+
+// Create a function to find area of rectangle. Consider length parameter as required and breadth 
+// parameter as optional.
+
+// const area = (length,breadth=6) => {
+//     console.log("Area of rectangle is: "+length*breadth);
+// }
+// area(5)
+
+// Create an anonymous function to return square of number.
+
+
+// const square= function(num){
+//     return num*num;
+// }
+// console.log(square(5))
+
+// create a arrow function to return cube of number.
+
+// const cube = (num) => (num*num*num);
+// console.log(cube(5))
+
+// Write a function to find area of circle using parametrized function.
+// function circleArea(radius){
+//     return Math.PI*radius*radius;
+// }
+// console.log("Area of a circle is: "+Math.round(circleArea(5)))
+
+// Write  a program to create function which return count of digits which number pass as parameter?
+// const digitNumber=(num) => (num.toString().length)
+// console.log(digitNumber(123));
