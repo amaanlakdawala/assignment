9735339e1a44132ed562d9ffed84e5f3fadc5274
amaanlakdawala <amaanlakdawala301@gmail.com>
2024-03-15 23:40:17 +0530

    assignment of prime number even ,odd and switch case

---
+++ js_ass2.js
@@ -0,0 +1,145 @@
+// a=13
+// var flag=false
+// for (let i=2;i<a;i++){
+//     if(a%i==0){
+//         flag=true
+//         break
+//     }
+// }
+// if(flag){
+//     console.log("the number "+a+" is not prime")
+// }
+// else{
+//     console.log("the number "+a+" is prime")
+// }
+
+
+// for (let i=10;i<101;i++){
+//     if(i%2!=0){
+//         console.log(i)
+//     }
+// }
+
+// a=10
+// if(a%2==0){
+//     console.log("Its Even")
+// }
+// else{
+//     console.log("Its Odd")
+// }
+
+
+
+// sum=0
+// for(let i =1 ; i<51; i++){
+//     sum=sum+i
+// }
+// console.log(sum)
+
+// n=50
+// sum=n*(n+1)/2
+// console.log(sum)
+
+
+// a=123
+// sum=0
+// while(a>0){
+//     n=a%10
+//     sum=sum+n
+//     a=a/10
+// }
+// console.log(Math.floor(sum))
+
+
+// for (let i=1; i<101; i++){
+//     if (i%5==0 && i>85){
+//         console.log(i)
+//     }
+// }
+
+
+// 371 
+// 120 not
+
+// a=371
+// c=a
+// str=a.toString().length
+// sum=0
+// for (let i=0; i<str;i++){
+//     n=a%10
+//     sum=sum+(n**str)  
+//     a=Math.floor(a/10)
+// }
+// if (sum==c){
+//     console.log("The number is Armstrong: "+c)
+// }
+// else{
+//     console.log("The number is not Armstrong: "+c)
+// }
+
+
+// num=prompt("enter number: ")
+// num1=parseInt(num)
+// if(num1<0){
+//     console.log("Number is Negative")
+// }
+// else if (num1>0){
+//     console.log("Number is Positive")
+// }
+// else{
+//     console.log("Number is Zero")
+// }
+
+
+
+// num=prompt("enter number: ")
+// num1=parseInt(num)
+// for (let i=1;i<=10;i++){
+//     document.writeln(num+" x"+i+" ="+num*i+"<br>")
+// }
+
+// function printDayOfWeek(day) {
+//     let dayName;
+//     switch (day) {
+//         case 0:
+//             dayName = "Sunday";
+//             break;
+//         case 1:
+//             dayName = "Monday";
+//             break;
+//         case 2:
+//             dayName = "Tuesday";
+//             break;
+//         case 3:
+//             dayName = "Wednesday";
+//             break;
+//         case 4:
+//             dayName = "Thursday";
+//             break;
+//         case 5:
+//             dayName = "Friday";
+//             break;
+//         case 6:
+//             dayName = "Saturday";
+//             break;
+//         default:
+//             dayName = "Invalid day";
+//             break;
+//     }
+//     return dayName;
+// }
+
+// const dayNumber = parseInt(prompt("Enter the day of the week: "));
+// const dayOfWeek = printDayOfWeek(dayNumber);
+// console.log("The day of the week is:", dayOfWeek);
+
+
+
+
+
+
+
+
+
+
+
