6b17d911b83472f6c2516481980e6bc717ebab32
amaanlakdawala <amaanlakdawala301@gmail.com>
2024-03-28 21:46:55 +0530

    file handling all concepts

---
+++ file/program.py
@@ -0,0 +1,31 @@
+
+
+# f=open("file/demo.txt","w")
+# f.write(" Line written ")
+# f.close()
+
+# f=open("file/demo.txt","a")
+# f.write("\n second line written")
+# f.close()
+
+# f=open("file/demo.txt","r")
+# data=f.readlines()
+# for line in data:
+#     print(line)
+# f.close()
+
+
+# f=open("file/demo1.txt","x")
+
+
+# import os
+# os.remove("file\demo1.txt")
+
+
+
+# import os
+# if os.path.exists("file\demo1.txt"):
+#     os.remove("file\demo1.txt")
+# else:
+#     print("file does not exists")
+
